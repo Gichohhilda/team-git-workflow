6409ff1a61a5a4aedc5767785392215df81beb4f
Hilda <hildagichohi27@gmail.com>
2026-05-08 18:14:43 +0300

    Added minor updates to the project files

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const actionBtn = document.getElementById("action-btn");
 
     actionBtn.addEventListener("click", () => {
+        // update the test
         alert("Button clicked! This is part of my Git practice.");
     });
 });
