b8fa053fc189095f1def23ec04665c799e29ac7c
Hilda <hildagichohi27@gmail.com>
2026-05-08 17:39:13 +0300

    Initial project setup

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const actionBtn = document.getElementById("action-btn");
+
+    actionBtn.addEventListener("click", () => {
+        alert("Button clicked! This is part of my Git practice.");
+    });
+});
